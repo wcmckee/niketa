8d0727ad58d2b6e77c008609079531278fdd5f23
wcmckee <wcmckee@localhost.localdomain>
2015-03-01 13:51:22 +1300

    tags script start

---
+++ wcmnikolatags.py
@@ -0,0 +1,31 @@
+
+# coding: utf-8
+
+# wcm Nikola Tags
+# 
+# convert ipynb/py doc imports as tags for nikola blog .meta files.
+# 
+# When user searches for notebook to blog with bbknikola python script also get the tags for the .meta file. Open up the .py file and convert this:
+# 
+# blogpost.py
+# 
+# import requests
+# import os
+# import re
+# 
+# Into:
+# 
+# blogpost.meta
+# 
+# blogpost
+# blogpost
+# 2015/02/31 00:00:00
+# requests, os, re
+
+# Categorie can be the name of the repo.
+# Need a repo for Nikola scripts
+
+# In[ ]:
+
+
+
